e298816224bce19aeaddefad0a9c67c5bd496347
oll310e <o.anderzen@gmail.com>
2023-01-05 09:19:25 +0100

    Create button eventListener and make buildGrid function

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 const container = document.querySelector('#container');
+const button = document.querySelector('#button');
 
-let gridSquares = 20;
+let gridSquares = 16;
 
 container.style = `height: ${gridSquares * 10}px; width: ${gridSquares * 10}px;`
 
-for(let i = 0; i < gridSquares * gridSquares; i++){
-    let newSquare = document.createElement('div');
-    newSquare.classList.add('square');
-    newSquare.addEventListener('mouseover', e => {
-        e.target.classList.add('painted');
-    });
-    container.appendChild(newSquare);
-    console.log(i)
-};
+const buildGrid = (gridSquares) => {
+    for(let i = 0; i < gridSquares * gridSquares; i++){
+        let newSquare = document.createElement('div');
+        newSquare.classList.add('square');
+        newSquare.addEventListener('mouseover', e => {
+            e.target.classList.add('painted');
+        });
+        container.appendChild(newSquare);
+    };
+}
+
+buildGrid(gridSquares);
+
+button.addEventListener('click', () => {
+    container.innerHTML = '';
+    let selectedSquares = prompt('How many squares is the heigth and width?');
+
+    buildGrid(selectedSquares);
+    container.style = `height: ${selectedSquares * 10}px; width: ${selectedSquares * 10}px;`
+})
